d1185441a1bfcbc8b738f04fe33e0e3fbc2054ff
davemackintosh <me@davemackintosh.co.uk>
2022-09-22 22:25:37 +0100

    Init

---
+++ cmd/v1/graphql-generator/internal/tag-parser/tag_parser.go
@@ -0,0 +1,115 @@
+package tagparser
+
+import (
+	"reflect"
+)
+
+// Tag represents a GraphQL tag.
+type Tag struct {
+	Name    string
+	Options map[string]string
+}
+
+// Get all the tags from a struct and deduce which are GraphQL related.
+func GetTagsFromStruct(structType reflect.Type) (map[string]Tag, error) {
+	tags := make(map[string]Tag)
+
+	for i := 0; i < structType.NumField(); i++ {
+		field := structType.Field(i)
+		tag, err := GetTagFromField(field)
+		if err != nil {
+			return nil, err
+		}
+
+		if tag != nil {
+			tags[tag.Name] = *tag
+		}
+	}
+
+	return tags, nil
+}
+
+// Get the tag from a Field.
+func GetTagFromField(field reflect.StructField) (*Tag, error) {
+	tag := field.Tag.Get("graphql")
+
+	if tag == "" {
+		return nil, nil
+	}
+
+	return ParseTag(tag, field.Name)
+}
+
+// Parse a tag.
+func ParseTag(tag string, fieldName string) (*Tag, error) {
+	// Loop over each character and assemble a map of tags,
+	// we do this because decorators and comments can contain commas
+	// and we don't want to split on those
+	tagOptions := make(map[string]string)
+
+	var (
+		currentKey   string
+		currentValue string
+		inQuotes     bool
+		escaped      bool
+		inArray      bool
+	)
+
+	for _, char := range tag {
+		if char == '[' {
+			inArray = true
+		}
+
+		if char == ']' {
+			inArray = false
+		}
+
+		if char == '\\' {
+			escaped = true
+
+			continue
+		}
+
+		if char == '"' && !escaped {
+			inQuotes = !inQuotes
+		}
+
+		if char == ',' && !inQuotes && !inArray {
+			if currentKey == "" {
+				currentKey = currentValue
+			}
+
+			tagOptions[currentKey] = currentValue
+			currentKey = ""
+			currentValue = ""
+
+			continue
+		}
+
+		if char == '=' {
+			currentKey = currentValue
+			currentValue = ""
+
+			continue
+		}
+
+		currentValue += string(char)
+	}
+
+	tagOptions[currentKey] = currentValue
+
+	// If the tag is empty, return nil
+	if len(tagOptions) == 0 {
+		return &Tag{Name: fieldName}, nil
+	}
+
+	// If the tag doesn't have a name, use the field field
+	if tagOptions["name"] == "" {
+		tagOptions["name"] = fieldName
+	}
+
+	return &Tag{
+		Name:    tagOptions["name"],
+		Options: tagOptions,
+	}, nil
+}
---
+++ cmd/v1/graphql-generator/internal/tag-parser/tag_parser_test.go
@@ -0,0 +1,53 @@
+package tagparser_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	tagparser "github.com/warpspeedboilerplate/graphql-schema-generator/cmd/v1/graphql-generator/internal/tag-parser"
+)
+
+type TestStruct struct {
+	TaggedField   string `graphql:"taggedField, omitempty, description=This is a tagged field, decorators=[+doc(description: \"This field is tagged.\"), +requireAuthRole(role: \"admin\"))]"`
+	UnTaggedField string
+}
+
+func TestGetTagsFromStruct(t *testing.T) {
+	tests := []struct {
+		name    string
+		s       interface{}
+		want    map[string]tagparser.Tag
+		wantErr bool
+	}{
+		{
+			name: "TestGetTagsFromStruct",
+			s:    TestStruct{},
+			want: map[string]tagparser.Tag{
+				"taggedField": {
+					Name: "taggedField",
+					Options: map[string]string{
+						"omitempty":   "omitempty",
+						"description": "This is a tagged field",
+						"decorators":  "[+doc(description: \"This field is tagged.\"), +requireAuthRole(role: \"admin\"))]",
+						"name":        "taggedField",
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tagparser.GetTagsFromStruct(reflect.TypeOf(tt.s))
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetTagsFromStruct() error = %v, wantErr %v", err, tt.wantErr)
+
+				return
+			}
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
